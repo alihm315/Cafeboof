f0651ad3f303246d438af8cecc1f5eac10bf2a7d
alihm315 <ali.hm315@gmail.com>
2026-01-04 19:43:06 +0330

    Update script.js

--- script.js
+++ script.js
@@ -132,4 +132,6 @@ document.getElementById('checkout').onclick = () => {
   cart = [];
   updateCart();
   cartPopup.classList.add('hidden');
+  function toggleRecipe(el){
+  el.nextElementSibling.classList.toggle('show');
 };
